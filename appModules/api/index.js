5fdac70c35d20bfe50eb1646859263d19fb89b10
sfkrgbkj <55603453+sfkrgbkj@users.noreply.github.com>
2024-05-01 13:57:39 +0300

    f
    f

---
+++ appModules/api/index.js
@@ -0,0 +1,5 @@
+const {getData} = require("./api-utils")
+const {getRandomGame} = require("./api-utils")
+const endpoints = require("./config.js")
+
+module.exports = {getData, endpoints, getRandomGame}
---
+++ controllers/index.js
@@ -0,0 +1,7 @@
+const defaultRouteController = require("./default")
+const mainRouteController = require("./main")
+const gameRouteController = require("./game")
+const voteRouteController = require("./vote")
+module.exports = {
+    defaultRouteController, mainRouteController,gameRouteController, voteRouteController
+}
