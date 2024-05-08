7974292e954d91b53bc51b7b16552540ee026e5b
sfkrgbkj <55603453+sfkrgbkj@users.noreply.github.com>
2024-05-08 23:26:19 +0300

    review

---
+++ kb-backend-week-2-startkit-main/appModules/http-utils/index.js
@@ -0,0 +1,5 @@
+const mimeTypes = require("./mime-types")
+const staticFIle = require("./static-file")
+const parseBody = require("./parse-body")
+
+module.exports = {mimeTypes, staticFIle, parseBody}
