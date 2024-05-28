dc3e98f8cccddc174684b2383004fa14cfd74120
sfkrgbkj <55603453+sfkrgbkj@users.noreply.github.com>
2024-05-28 17:16:07 +0300

    Delete appModules directory

--- appModules/api/index.js
+++
@@ -1,5 +0,0 @@
-const {getData} = require("./api-utils")
-const {getRandomGame} = require("./api-utils")
-const endpoints = require("./config.js")
-
-module.exports = {getData, endpoints, getRandomGame}
--- appModules/http-utils/index.js
+++
@@ -1,5 +0,0 @@
-const mimeTypes = require("./mime-types")
-const staticFIle = require("./static-file")
-const parseBody = require("./parse-body")
-
-module.exports = {mimeTypes, staticFIle, parseBody}
