1ce347038a0be45bdfb94cd4bb1a1c5319c474ba
sfkrgbkj <55603453+sfkrgbkj@users.noreply.github.com>
2024-05-28 17:16:58 +0300

    Delete controllers directory

--- controllers/index.js
+++
@@ -1,7 +0,0 @@
-const defaultRouteController = require("./default")
-const mainRouteController = require("./main")
-const gameRouteController = require("./game")
-const voteRouteController = require("./vote")
-module.exports = {
-    defaultRouteController, mainRouteController,gameRouteController, voteRouteController
-}
